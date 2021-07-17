bb086bf22a553b9221758e95db12364d7de5e1c4
Naleakan <naleakan@protonmail.com>
2021-07-17 19:32:18 +0200

    Web server initialization
    Initialization of the web server

---
+++ server/web/web.go
@@ -0,0 +1,31 @@
+package web
+
+import (
+	"naleakan/stormtask/configuration"
+	"naleakan/stormtask/database"
+
+	"github.com/gorilla/mux"
+)
+
+type Server struct {
+	Router   *mux.Router
+	Database *database.DBHandler
+}
+
+// InitServer initializes the http server and the database
+func InitServer(configuration configuration.ConfStruct) (*Server, error) {
+	router := mux.NewRouter()
+	db, err := database.Init(configuration.DatabaseUrl, configuration.DatabaseUser, configuration.DatabasePassword, configuration.DatabaseName)
+	if err != nil {
+		return nil, err
+	}
+	return &Server{
+		Router:   router,
+		Database: db,
+	}, nil
+}
+
+// Close close the server connection
+func (server *Server) Close() error {
+	return server.Database.Close()
+}
