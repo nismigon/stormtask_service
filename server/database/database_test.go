5116332648cff0ded140455e804ce2297bc69fe9
Teissem <teissedre.mathieu@outlook.com>
2021-08-26 23:58:06 +0200

    Testify module to improve the tests

--- server/database/database_test.go
+++ server/database/database_test.go
@@ -3,86 +3,96 @@ package database
 import (
 	"teissem/stormtask/server/configuration"
 	"testing"
+
+	"github.com/stretchr/testify/suite"
 )
 
-func TestInitRight(t *testing.T) {
+type DatabaseTestSuite struct {
+	suite.Suite
+}
+
+func (suite *DatabaseTestSuite) TestInit() {
 	conf, err := configuration.Parse("../../configuration.json")
 	if err != nil {
-		t.Errorf("Failed to parse the configuration file : " + err.Error())
+		suite.T().Errorf("Failed to parse the configuration file : " + err.Error())
 	}
 	handler, err := Init(conf.DatabaseURL, conf.DatabaseUser, conf.DatabasePassword, conf.DatabaseName)
 	if err != nil {
-		t.Errorf("Failed to init the database connection : " + err.Error())
+		suite.T().Errorf("Failed to init the database connection : " + err.Error())
 	} else {
 		err = handler.Close()
 		if err != nil {
-			t.Errorf("Failed to close the database : " + err.Error())
+			suite.T().Errorf("Failed to close the database : " + err.Error())
 		}
 	}
 }
 
-func TestInitWrongURL(t *testing.T) {
+func (suite *DatabaseTestSuite) TestInitWrongURL() {
 	conf, err := configuration.Parse("../../configuration.json")
 	if err != nil {
-		t.Errorf("Failed to parse the configuration file : " + err.Error())
-		t.FailNow()
+		suite.T().Errorf("Failed to parse the configuration file : " + err.Error())
+		suite.T().FailNow()
 	}
 	handler, err := Init("toto", conf.DatabaseUser, conf.DatabasePassword, conf.DatabaseName)
 	if err == nil {
-		t.Errorf("This test should fail : Wrong database URL given")
+		suite.T().Errorf("This test should fail : Wrong database URL given")
 		err = handler.Close()
 		if err != nil {
-			t.Errorf("Failed to close the database : " + err.Error())
+			suite.T().Errorf("Failed to close the database : " + err.Error())
 		}
-		t.FailNow()
+		suite.T().FailNow()
 	}
 }
 
-func TestInitWrongUser(t *testing.T) {
+func (suite *DatabaseTestSuite) TestInitWrongUser() {
 	conf, err := configuration.Parse("../../configuration.json")
 	if err != nil {
-		t.Errorf("Failed to parse the configuration file : " + err.Error())
-		t.FailNow()
+		suite.T().Errorf("Failed to parse the configuration file : " + err.Error())
+		suite.T().FailNow()
 	}
 	handler, err := Init(conf.DatabaseURL, "toto", conf.DatabasePassword, conf.DatabaseName)
 	if err == nil {
-		t.Errorf("This test should fail : Wrong database user given")
+		suite.T().Errorf("This test should fail : Wrong database user given")
 		err = handler.Close()
 		if err != nil {
-			t.Errorf("Failed to close the database : " + err.Error())
+			suite.T().Errorf("Failed to close the database : " + err.Error())
 		}
-		t.FailNow()
+		suite.T().FailNow()
 	}
 }
 
-func TestInitWrongPassword(t *testing.T) {
+func (suite *DatabaseTestSuite) TestInitWrongPassword() {
 	conf, err := configuration.Parse("../../configuration.json")
 	if err != nil {
-		t.Errorf("Failed to parse the configuration file : " + err.Error())
+		suite.T().Errorf("Failed to parse the configuration file : " + err.Error())
 	}
 	handler, err := Init(conf.DatabaseURL, conf.DatabaseUser, "password", conf.DatabaseName)
 	if err == nil {
-		t.Errorf("This test should fail : Wrong database password given")
+		suite.T().Errorf("This test should fail : Wrong database password given")
 		err = handler.Close()
 		if err != nil {
-			t.Errorf("Failed to close the database : " + err.Error())
+			suite.T().Errorf("Failed to close the database : " + err.Error())
 		}
-		t.FailNow()
+		suite.T().FailNow()
 	}
 }
 
-func TestInitWrongName(t *testing.T) {
+func (suite *DatabaseTestSuite) TestInitWrongName() {
 	conf, err := configuration.Parse("../../configuration.json")
 	if err != nil {
-		t.Errorf("Failed to parse the configuration file : " + err.Error())
+		suite.T().Errorf("Failed to parse the configuration file : " + err.Error())
 	}
 	handler, err := Init(conf.DatabaseURL, conf.DatabaseUser, conf.DatabasePassword, "toto")
 	if err == nil {
-		t.Errorf("This test should fail : Wrong database name given")
+		suite.T().Errorf("This test should fail : Wrong database name given")
 		err = handler.Close()
 		if err != nil {
-			t.Errorf("Failed to close the database : " + err.Error())
+			suite.T().Errorf("Failed to close the database : " + err.Error())
 		}
-		t.FailNow()
+		suite.T().FailNow()
 	}
+}
+
+func TestDatabase(t *testing.T) {
+	suite.Run(t, new(DatabaseTestSuite))
 }
