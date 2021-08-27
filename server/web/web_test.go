2ddf79c380a1ce8b61794f6ef374abc4016dc4c1
Teissem <teissedre.mathieu@outlook.com>
2021-08-27 21:43:03 +0200

    Switch to testify on web/web_test

--- server/web/web_test.go
+++ server/web/web_test.go
@@ -3,19 +3,29 @@ package web
 import (
 	"teissem/stormtask/server/configuration"
 	"testing"
+
+	"github.com/stretchr/testify/suite"
 )
 
-func TestInitAndCloseServer(t *testing.T) {
+type WebTestSuite struct {
+	suite.Suite
+}
+
+func (suite *WebTestSuite) TestInitAndCloseServer() {
 	conf, err := configuration.Parse("../../configuration.json")
 	if err != nil {
-		t.Errorf("Failed to parse the configuration file : " + err.Error())
+		suite.T().Errorf("Failed to parse the configuration file : " + err.Error())
 	}
 	server, err := InitServer(*conf)
 	if err != nil {
-		t.Errorf("Failed to init the server : " + err.Error())
+		suite.T().Errorf("Failed to init the server : " + err.Error())
 	}
 	err = server.Close()
 	if err != nil {
-		t.Errorf("Failed to close the server : " + err.Error())
+		suite.T().Errorf("Failed to close the server : " + err.Error())
 	}
+}
+
+func TestWeb(t *testing.T) {
+	suite.Run(t, new(WebTestSuite))
 }
