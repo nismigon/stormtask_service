0afdae59540da5f7d780e2fcc8b3dd37d54ebbf4
Teissem <teissedre.mathieu@outlook.com>
2021-08-27 20:44:26 +0200

    Switch to testify on web/auth_test

--- server/web/auth_test.go
+++ server/web/auth_test.go
@@ -3,114 +3,117 @@ package web
 import (
 	"teissem/stormtask/server/configuration"
 	"testing"
+
+	"github.com/stretchr/testify/suite"
 )
 
-func TestGenerateTokenRight(t *testing.T) {
+type AuthTestSuite struct {
+	suite.Suite
+	Server *Server
+}
+
+func (suite *AuthTestSuite) SetupTest() {
 	conf, err := configuration.Parse("../../configuration.json")
 	if err != nil {
-		t.Errorf("Failed to parse the configuration file : " + err.Error())
+		suite.T().Errorf("Failed to parse the configuration file : " + err.Error())
 	}
 	server, err := InitServer(*conf)
 	if err != nil {
-		t.Errorf("Failed to init the server : " + err.Error())
+		suite.T().Errorf("Failed to init the server : " + err.Error())
 	}
-	user, err := server.Database.AddUser("test@test.com", "Test", "Test", false)
+	suite.Server = server
+}
+
+func (suite *AuthTestSuite) TearDownTest() {
+	_ = suite.Server.Close()
+}
+
+func (suite *AuthTestSuite) TestGenerateTokenRight() {
+	user, err := suite.Server.Database.AddUser("test@test.com", "Test", "Test", false)
 	if err != nil {
-		t.Errorf("Failed to add the user : " + err.Error())
+		suite.T().Errorf("Failed to add the user : " + err.Error())
 	}
 	var cred = Credentials{
 		Email:    user.Email,
 		Password: user.Password,
 	}
-	token, err := server.generateToken(cred)
+	token, err := suite.Server.generateToken(cred)
 	if err != nil {
-		t.Errorf("Failed to generate the token : " + err.Error())
+		suite.T().Errorf("Failed to generate the token : " + err.Error())
 	}
 	if token == "" {
-		t.Errorf("Failed to generate the token")
+		suite.T().Errorf("Failed to generate the token")
 	}
-	claims, err := server.ValidateAndExtractToken(token)
+	claims, err := suite.Server.ValidateAndExtractToken(token)
 	if err != nil {
-		t.Errorf("Failed to validate and extract the token : " + err.Error())
+		suite.T().Errorf("Failed to validate and extract the token : " + err.Error())
 	}
 	if claims == nil {
-		t.Errorf("Failed to validate and extract the token : The token is invalid")
+		suite.T().Errorf("Failed to validate and extract the token : The token is invalid")
 	} else {
 		if claims.ID != user.ID {
-			t.Errorf("Failed to assign the ID in the token")
+			suite.T().Errorf("Failed to assign the ID in the token")
 		}
 		if claims.Email != user.Email {
-			t.Errorf("Failed to assign the email in the token")
+			suite.T().Errorf("Failed to assign the email in the token")
 		}
 		if claims.Name != user.Name {
-			t.Errorf("Failed to assign the name in the token")
+			suite.T().Errorf("Failed to assign the name in the token")
 		}
 		if claims.IsAdmin != user.IsAdmin {
-			t.Errorf("Failed to assign the is admin in the token")
+			suite.T().Errorf("Failed to assign the is admin in the token")
 		}
 	}
-	err = server.Database.DeleteUser(user.ID)
+	err = suite.Server.Database.DeleteUser(user.ID)
 	if err != nil {
-		t.Errorf("Failed to delete the user : " + err.Error())
+		suite.T().Errorf("Failed to delete the user : " + err.Error())
 	}
 }
 
-func TestGenerateTokenWrongEmail(t *testing.T) {
-	conf, err := configuration.Parse("../../configuration.json")
+func (suite *AuthTestSuite) TestGenerateTokenWrongEmail() {
+	user, err := suite.Server.Database.AddUser("test@test.com", "Test", "Test", false)
 	if err != nil {
-		t.Errorf("Failed to parse the configuration file : " + err.Error())
-	}
-	server, err := InitServer(*conf)
-	if err != nil {
-		t.Errorf("Failed to init the server : " + err.Error())
-	}
-	user, err := server.Database.AddUser("test@test.com", "Test", "Test", false)
-	if err != nil {
-		t.Errorf("Failed to add the user : " + err.Error())
+		suite.T().Errorf("Failed to add the user : " + err.Error())
 	}
 	var cred = Credentials{
 		Email:    "toto@toto.com",
 		Password: user.Password,
 	}
-	token, err := server.generateToken(cred)
+	token, err := suite.Server.generateToken(cred)
 	if err != nil {
-		t.Errorf("Failed to generate the token : " + err.Error())
+		suite.T().Errorf("Failed to generate the token : " + err.Error())
 	}
 	if token != "" {
-		t.Errorf("Failed to generate the token : A wrong email give a correct token")
+		suite.T().Errorf("Failed to generate the token : A wrong email give a correct token")
 	}
-	err = server.Database.DeleteUser(user.ID)
+	err = suite.Server.Database.DeleteUser(user.ID)
 	if err != nil {
-		t.Errorf("Failed to delete the user : " + err.Error())
+		suite.T().Errorf("Failed to delete the user : " + err.Error())
 	}
 }
 
-func TestGenerateTokenWrongPassword(t *testing.T) {
-	conf, err := configuration.Parse("../../configuration.json")
+func (suite *AuthTestSuite) TestGenerateTokenWrongPassword() {
+	user, err := suite.Server.Database.AddUser("test@test.com", "Test", "Test", false)
 	if err != nil {
-		t.Errorf("Failed to parse the configuration file : " + err.Error())
-	}
-	server, err := InitServer(*conf)
-	if err != nil {
-		t.Errorf("Failed to init the server : " + err.Error())
-	}
-	user, err := server.Database.AddUser("test@test.com", "Test", "Test", false)
-	if err != nil {
-		t.Errorf("Failed to add the user : " + err.Error())
+		suite.T().Errorf("Failed to add the user : " + err.Error())
 	}
 	var cred = Credentials{
 		Email:    user.Email,
 		Password: "toto",
 	}
-	token, err := server.generateToken(cred)
+	token, err := suite.Server.generateToken(cred)
 	if err != nil {
-		t.Errorf("Failed to generate the token : " + err.Error())
+		suite.T().Errorf("Failed to generate the token : " + err.Error())
 	}
 	if token != "" {
-		t.Errorf("Failed to generate the token : A wrong password give a correct token")
+		suite.T().Errorf("Failed to generate the token : A wrong password give a correct token")
 	}
-	err = server.Database.DeleteUser(user.ID)
+	err = suite.Server.Database.DeleteUser(user.ID)
 	if err != nil {
-		t.Errorf("Failed to delete the user : " + err.Error())
+		suite.T().Errorf("Failed to delete the user : " + err.Error())
 	}
+}
+
+func TestAuth(t *testing.T) {
+	suite.Run(t, new(AuthTestSuite))
 }
