82b1f7ee8f180ab2858dc01a27d1406e90feb2df
Teissem <teissedre.mathieu@outlook.com>
2021-08-27 20:10:36 +0200

    Switch to testify on database/task_test

--- server/database/task_test.go
+++ server/database/task_test.go
@@ -3,278 +3,198 @@ package database
 import (
 	"teissem/stormtask/server/configuration"
 	"testing"
+
+	"github.com/stretchr/testify/suite"
 )
 
-func BeforeTaskTest() (*DBHandler, int, error) {
+type TaskTestSuite struct {
+	suite.Suite
+	Handler *DBHandler
+	User    *UserInformation
+	Group   *GroupInformation
+	Task    *TaskInformation
+}
+
+func (suite *TaskTestSuite) SetupTest() {
 	conf, err := configuration.Parse("../../configuration.json")
 	if err != nil {
-		return nil, -1, err
+		suite.T().Errorf("Failed to parse the configuration file : " + err.Error())
 	}
 	handler, err := Init(conf.DatabaseURL, conf.DatabaseUser, conf.DatabasePassword, conf.DatabaseName)
 	if err != nil {
-		return nil, -1, err
+		suite.T().Errorf("Failed to initialize the connection with the database : " + err.Error())
 	}
+	suite.Handler = handler
 	user, err := handler.AddUser("test@test.com", "Test", "Test", false)
 	if err != nil {
-		return nil, -1, err
+		suite.T().Errorf("Failed to add the user in the database : " + err.Error())
 	}
+	suite.User = user
 	group, err := handler.AddGroup(user.ID, "TestGroup")
 	if err != nil {
-		return nil, -1, err
+		suite.T().Errorf("Failed to add the group in the database : " + err.Error())
 	}
-	return handler, group.ID, nil
+	suite.Group = group
+	task, err := handler.AddTask("TestTask", "Description of the task", false, false, group.ID)
+	if err != nil {
+		suite.T().Errorf("Failed to add the task in the database : " + err.Error())
+	}
+	suite.Task = task
 }
 
-func AfterTaskTest(handler *DBHandler, id int) {
-	group, _ := handler.GetGroupByID(id)
-	_ = handler.DeleteGroup(id)
-	_ = handler.DeleteUser(group.UserID)
-	_ = handler.Close()
+func (suite *TaskTestSuite) TearDownTest() {
+	err := suite.Handler.DeleteUser(suite.User.ID)
+	if err != nil {
+		suite.T().Errorf("Failed to delete user : " + err.Error())
+	}
+	_ = suite.Handler.Close()
 }
 
-func TestAddAndDeleteTask(t *testing.T) {
-	handler, groupID, err := BeforeTaskTest()
+func (suite *TaskTestSuite) TestAddAndDeleteTask() {
+	task, err := suite.Handler.AddTask("Task", "A little description", false, false, suite.Group.ID)
 	if err != nil {
-		t.Errorf("Failed to initialize the task test, please see other test to see what happens : " + err.Error())
-	}
-	task, err := handler.AddTask("Task", "A little description", false, false, groupID)
-	if err != nil {
-		t.Errorf("Failed to add the task : " + err.Error())
+		suite.T().Errorf("Failed to add the task : " + err.Error())
 	}
 	if task.Name != "Task" {
-		t.Errorf("Failed to set the name of the task")
+		suite.T().Errorf("Failed to set the name of the task")
 	}
 	if task.Description != "A little description" {
-		t.Errorf("Failed to set the description of the task")
+		suite.T().Errorf("Failed to set the description of the task")
 	}
 	if task.IsArchived {
-		t.Errorf("Failed to set the archived status of the task")
+		suite.T().Errorf("Failed to set the archived status of the task")
 	}
 	if task.IsFinished {
-		t.Errorf("Failed to set the finished status of the task")
+		suite.T().Errorf("Failed to set the finished status of the task")
 	}
-	if task.IDGroup != groupID {
-		t.Errorf("Failed to set the owner group of the task")
+	if task.IDGroup != suite.Group.ID {
+		suite.T().Errorf("Failed to set the owner group of the task")
 	}
-	err = handler.DeleteTask(task.ID)
+	err = suite.Handler.DeleteTask(task.ID)
 	if err != nil {
-		t.Errorf("Failed to delete the task : " + err.Error())
+		suite.T().Errorf("Failed to delete the task : " + err.Error())
 	}
-	AfterTaskTest(handler, groupID)
 }
 
-func TestAddTaskWrongGroup(t *testing.T) {
-	handler, groupID, err := BeforeTaskTest()
-	if err != nil {
-		t.Errorf("Failed to initialize the task test, please see other test to see what happens : " + err.Error())
-	}
-	_, err = handler.AddTask("Task", "A little description", false, false, -1)
+func (suite *TaskTestSuite) TestAddTaskWrongGroup() {
+	_, err := suite.Handler.AddTask("Task", "A little description", false, false, -1)
 	if err == nil {
-		t.Errorf("Failed to add the task : no error when a wrong group is given")
+		suite.T().Errorf("Failed to add the task : no error when a wrong group is given")
 	}
-	AfterTaskTest(handler, groupID)
 }
 
-func TestModifyTaskRight(t *testing.T) {
-	handler, groupID, err := BeforeTaskTest()
+func (suite *TaskTestSuite) TestModifyTaskRight() {
+	task, err := suite.Handler.ModifyTask(suite.Task.ID, "New Task", "A bigger description", true, true, suite.Task.IDGroup)
 	if err != nil {
-		t.Errorf("Failed to initialize the task test, please see other test to see what happens : " + err.Error())
-	}
-	tmpTask, err := handler.AddTask("Task", "A little description", false, false, groupID)
-	if err != nil {
-		t.Errorf("Failed to add the task : " + err.Error())
-	}
-	task, err := handler.ModifyTask(tmpTask.ID, "New Task", "A bigger description", true, true, tmpTask.IDGroup)
-	if err != nil {
-		t.Errorf("Failed to modify task : " + err.Error())
+		suite.T().Errorf("Failed to modify task : " + err.Error())
 	}
 	if task.Name != "New Task" {
-		t.Errorf("Failed to set the name of the task")
+		suite.T().Errorf("Failed to set the name of the task")
 	}
 	if task.Description != "A bigger description" {
-		t.Errorf("Failed to set the description of the task")
+		suite.T().Errorf("Failed to set the description of the task")
 	}
 	if !task.IsArchived {
-		t.Errorf("Failed to set the archived status of the task")
+		suite.T().Errorf("Failed to set the archived status of the task")
 	}
 	if !task.IsFinished {
-		t.Errorf("Failed to set the finished status of the task")
+		suite.T().Errorf("Failed to set the finished status of the task")
 	}
-	if task.IDGroup != groupID {
-		t.Errorf("Failed to set the owner group of the task")
+	if task.IDGroup != suite.Task.IDGroup {
+		suite.T().Errorf("Failed to set the owner group of the task")
 	}
-	err = handler.DeleteTask(task.ID)
-	if err != nil {
-		t.Errorf("Failed to delete the task : " + err.Error())
-	}
-	AfterTaskTest(handler, groupID)
 }
 
-func TestModifyTaskWrongID(t *testing.T) {
-	handler, groupID, err := BeforeTaskTest()
-	if err != nil {
-		t.Errorf("Failed to initialize the task test, please see other test to see what happens : " + err.Error())
-	}
-	_, err = handler.ModifyTask(-1, "Test", "Test", false, false, groupID)
+func (suite *TaskTestSuite) TestModifyTaskWrongID() {
+	_, err := suite.Handler.ModifyTask(-1, "Test", "Test", false, false, suite.Task.IDGroup)
 	if err == nil {
-		t.Errorf("Failed to modify task : give a wrong ID doesn't return an error")
+		suite.T().Errorf("Failed to modify task : give a wrong ID doesn't return an error")
 	}
-	AfterTaskTest(handler, groupID)
 }
 
-func TestModifyTaskWrongGroupID(t *testing.T) {
-	handler, groupID, err := BeforeTaskTest()
-	if err != nil {
-		t.Errorf("Failed to initialize the task test, please see other test to see what happens : " + err.Error())
-	}
-	tmpTask, err := handler.AddTask("Test", "Test", false, false, groupID)
-	if err != nil {
-		t.Errorf("Failed to add task : " + err.Error())
-	}
-	_, err = handler.ModifyTask(tmpTask.ID, "Test", "Test", false, false, -1)
+func (suite *TaskTestSuite) TestModifyTaskWrongGroupID() {
+	_, err := suite.Handler.ModifyTask(suite.Task.ID, "Test", "Test", false, false, -1)
 	if err == nil {
-		t.Errorf("Failed to modify task : give a wrong group ID doesn't return an error")
+		suite.T().Errorf("Failed to modify task : give a wrong group ID doesn't return an error")
 	}
-	err = handler.DeleteTask(tmpTask.ID)
-	if err != nil {
-		t.Errorf("Failed to delete task : " + err.Error())
-	}
-	AfterTaskTest(handler, groupID)
 }
 
-func TestGetTaskByIDRight(t *testing.T) {
-	handler, groupID, err := BeforeTaskTest()
+func (suite *TaskTestSuite) TestGetTaskByIDRight() {
+	task, err := suite.Handler.GetTaskByID(suite.Task.ID)
 	if err != nil {
-		t.Errorf("Failed to initialize the task test, please see other test to see what happens : " + err.Error())
+		suite.T().Errorf("Failed to get task by its ID : " + err.Error())
 	}
-	tmpTask, err := handler.AddTask("Test", "Description", false, false, groupID)
-	if err != nil {
-		t.Errorf("Failed to add task : " + err.Error())
+	if task.Name != suite.Task.Name {
+		suite.T().Errorf("Failed to set the name of the task")
 	}
-	task, err := handler.GetTaskByID(tmpTask.ID)
-	if err != nil {
-		t.Errorf("Failed to get task by its ID : " + err.Error())
+	if task.Description != suite.Task.Description {
+		suite.T().Errorf("Failed to set the description of the task")
 	}
-	if task.Name != tmpTask.Name {
-		t.Errorf("Failed to set the name of the task")
+	if task.IsArchived != suite.Task.IsArchived {
+		suite.T().Errorf("Failed to set the archived status of the task")
 	}
-	if task.Description != tmpTask.Description {
-		t.Errorf("Failed to set the description of the task")
+	if task.IsFinished != suite.Task.IsFinished {
+		suite.T().Errorf("Failed to set the finished status of the task")
 	}
-	if task.IsArchived != tmpTask.IsArchived {
-		t.Errorf("Failed to set the archived status of the task")
+	if task.IDGroup != suite.Task.IDGroup {
+		suite.T().Errorf("Failed to set the owner group of the task")
 	}
-	if task.IsFinished != tmpTask.IsFinished {
-		t.Errorf("Failed to set the finished status of the task")
-	}
-	if task.IDGroup != tmpTask.IDGroup {
-		t.Errorf("Failed to set the owner group of the task")
-	}
-	err = handler.DeleteTask(tmpTask.ID)
-	if err != nil {
-		t.Errorf("Failed to delete task : " + err.Error())
-	}
-	AfterTaskTest(handler, groupID)
 }
 
-func TestGetTaskByIDWrongID(t *testing.T) {
-	handler, groupID, err := BeforeTaskTest()
-	if err != nil {
-		t.Errorf("Failed to initialize the task test, please see other test to see what happens : " + err.Error())
-	}
-	_, err = handler.GetTaskByID(-1)
+func (suite *TaskTestSuite) TestGetTaskByIDWrongID() {
+	_, err := suite.Handler.GetTaskByID(-1)
 	if err == nil {
-		t.Errorf("Failed to get task by its ID : No error whereas wrong task ID given")
+		suite.T().Errorf("Failed to get task by its ID : No error whereas wrong task ID given")
 	}
-	AfterTaskTest(handler, groupID)
 }
 
-func TestGetTaskByGroupRight(t *testing.T) {
-	handler, groupID, err := BeforeTaskTest()
+func (suite *TaskTestSuite) TestGetTaskByGroupRight() {
+	_, err := suite.Handler.AddTask("Test", "Description", false, false, suite.Group.ID)
 	if err != nil {
-		t.Errorf("Failed to initialize the task test, please see other test to see what happens : " + err.Error())
+		suite.T().Errorf("Failed to add task : " + err.Error())
 	}
-	tmpTask, err := handler.AddTask("Test", "Description", false, false, groupID)
+	tasks, err := suite.Handler.GetTasksByGroup(suite.Group.ID)
 	if err != nil {
-		t.Errorf("Failed to add task : " + err.Error())
+		suite.T().Errorf("Failed to get tasks by group : " + err.Error())
 	}
-	tasks, err := handler.GetTasksByGroup(groupID)
-	if err != nil {
-		t.Errorf("Failed to get tasks by group : " + err.Error())
+	if len(*tasks) != 2 {
+		suite.T().Errorf("Failed to get tasks by group : The result of the get task by group is different of 1")
 	}
-	if len(*tasks) != 1 {
-		t.Errorf("Failed to get tasks by group : The result of the get task by group is different of 1")
-	}
-	task := (*tasks)[0]
-	if task.Name != tmpTask.Name {
-		t.Errorf("Failed to set the name of the task")
-	}
-	if task.Description != tmpTask.Description {
-		t.Errorf("Failed to set the description of the task")
-	}
-	if task.IsArchived != tmpTask.IsArchived {
-		t.Errorf("Failed to set the archived status of the task")
-	}
-	if task.IsFinished != tmpTask.IsFinished {
-		t.Errorf("Failed to set the finished status of the task")
-	}
-	if task.IDGroup != tmpTask.IDGroup {
-		t.Errorf("Failed to set the owner group of the task")
-	}
-	err = handler.DeleteTask(tmpTask.ID)
-	if err != nil {
-		t.Errorf("Failed to delete task : " + err.Error())
-	}
-	AfterTaskTest(handler, groupID)
 }
 
-func TestDBHandler_DeleteTasksByGroup(t *testing.T) {
-	handler, groupID, err := BeforeTaskTest()
+func (suite *TaskTestSuite) TestDBHandler_DeleteTasksByGroup() {
+	_, err := suite.Handler.AddTask("Test1", "Description", false, false, suite.Group.ID)
 	if err != nil {
-		t.Errorf("Failed to initialize the task test, please see other test to see what happens : " + err.Error())
+		suite.T().Errorf("Failed to add task : " + err.Error())
 	}
-	_, err = handler.AddTask("Test1", "Description", false, false, groupID)
+	_, err = suite.Handler.AddTask("Test2", "Description", false, false, suite.Group.ID)
 	if err != nil {
-		t.Errorf("Failed to add task : " + err.Error())
+		suite.T().Errorf("Failed to add task : " + err.Error())
 	}
-	_, err = handler.AddTask("Test2", "Description", false, false, groupID)
+	err = suite.Handler.DeleteTasksByGroup(suite.Group.ID)
 	if err != nil {
-		t.Errorf("Failed to add task : " + err.Error())
+		suite.T().Errorf("Failed to delete tasks of the group : " + err.Error())
 	}
-	err = handler.DeleteTasksByGroup(groupID)
+	tasks, err := suite.Handler.GetTasksByGroup(suite.Group.ID)
 	if err != nil {
-		t.Errorf("Failed to delete tasks of the group : " + err.Error())
-	}
-	tasks, err := handler.GetTasksByGroup(groupID)
-	if err != nil {
-		t.Errorf("Failed to get the tasks for a selected group : " + err.Error())
+		suite.T().Errorf("Failed to get the tasks for a selected group : " + err.Error())
 	}
 	if len(*tasks) != 0 {
-		t.Errorf("Failed to delete the tasks : The returned array shoul be empty")
+		suite.T().Errorf("Failed to delete the tasks : The returned array shoul be empty")
 	}
-	AfterTaskTest(handler, groupID)
 }
 
-func TestUniqueConstraintTask(t *testing.T) {
-	handler, groupID, err := BeforeTaskTest()
-	if err != nil {
-		t.Errorf("Failed to initialize the task test, please see other test to see what happens : " + err.Error())
-	}
-	tmpTask, err := handler.AddTask("Test", "Description", false, false, groupID)
-	if err != nil {
-		t.Errorf("Failed to add task : " + err.Error())
-	}
-	task, err := handler.AddTask("Test", "An other description", false, false, groupID)
+func (suite *TaskTestSuite) TestUniqueConstraintTask() {
+	task, err := suite.Handler.AddTask("TestTask", "An other description", false, false, suite.Group.ID)
 	if err == nil {
-		t.Errorf("Failed to add task : no error whereas break unique constraint on name")
-		err = handler.DeleteTask(task.ID)
+		suite.T().Errorf("Failed to add task : no error whereas break unique constraint on name")
+		err = suite.Handler.DeleteTask(task.ID)
 		if err != nil {
-			t.Errorf("Failed to delete task : " + err.Error())
+			suite.T().Errorf("Failed to delete task : " + err.Error())
 		}
 	}
-	err = handler.DeleteTask(tmpTask.ID)
-	if err != nil {
-		t.Errorf("Failed to delete task : " + err.Error())
-	}
-	AfterTaskTest(handler, groupID)
+}
+
+func TestTask(t *testing.T) {
+	suite.Run(t, new(TaskTestSuite))
 }
